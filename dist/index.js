8ed064d9bde0546206c853917d508ab8b8cdd086
Daniel Schulz da Silva <162481701+danielschulz07@users.noreply.github.com>
2026-06-10 10:57:12 -0300

    add das APIs

--- dist/index.js
+++ dist/index.js
@@ -23,5 +23,37 @@ async function displayDadosCep(cep) {
         console.log(`Erro na API ViaCep\n${error.name}: ${error.message}`);
     }
 }
-console.log(consultarCEP("29650000"));
+async function consultaCNPJ(cnpj) {
+    const url = `https://receitaws.com.br/v1/cnpj/${cnpj}`;
+    const response = await fetch(url);
+    if (response.ok) {
+        return response.json();
+    }
+    throw new Error(`HTTP error! Status: ${response.status}`);
+}
+async function displayDadosCnpj(cnpjs) {
+    for (const cnpj of cnpjs) {
+        try {
+            const dados = await consultaCNPJ(cnpj);
+            if (dados.status === "ERROR") {
+                console.log(`CNPJ não encontrado: ${dados.message}`);
+            }
+            else {
+                console.log(`
+Empresa: ${dados.nome}
+Fantasia: ${dados.fantasia}
+CNPJ: ${dados.cnpj}
+Situação: ${dados.situacao}
+                `);
+            }
+        }
+        catch (error) {
+            console.log(`Erro ao consultar ${cnpj}`);
+            console.log(`${error.name}: ${error.message}`);
+        }
+    }
+}
+const cnpjs = ["45997418000153", "17140820000262"];
+displayDadosCep("29665000");
+displayDadosCnpj(cnpjs);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 async function consultarCEP(cep: string): Promise<JSON> {
     const url = `https://viacep.com.br/ws/${cep}/json/`;
     const response: Response = await fetch(url);
-    
+
     if (response.ok) {
         return response.json();
     } else {
@@ -12,7 +12,7 @@ async function displayDadosCep(cep: string): Promise<void> {
 
     try {
         const dados: JSON | any = await consultarCEP(cep);
-        
+
         if ("erro" in dados) {
             console.log("CEP não encontrado!");
         } else {
@@ -22,4 +22,41 @@ async function displayDadosCep(cep: string): Promise<void> {
         console.log(`Erro na API ViaCep\n${error.name}: ${error.message}`);
     }
 }
-console.log(consultarCEP("29650000"))
+
+async function consultaCNPJ(cnpj: string): Promise<any> {
+    const url = `https://receitaws.com.br/v1/cnpj/${cnpj}`;
+    const response: Response = await fetch(url);
+
+    if (response.ok) {
+        return response.json();
+    }
+
+    throw new Error(`HTTP error! Status: ${response.status}`);
+}
+
+async function displayDadosCnpj(cnpjs: string[]): Promise<void> {
+    for (const cnpj of cnpjs) {
+        try {
+            const dados = await consultaCNPJ(cnpj);
+
+            if (dados.status === "ERROR") {
+                console.log(`CNPJ não encontrado: ${dados.message}`);
+            } else {
+                console.log(`
+Empresa: ${dados.nome}
+Fantasia: ${dados.fantasia}
+CNPJ: ${dados.cnpj}
+Situação: ${dados.situacao}
+                `);
+            }
+        } catch (error: any) {
+            console.log(`Erro ao consultar ${cnpj}`);
+            console.log(`${error.name}: ${error.message}`);
+        }
+    }
+}
+
+const cnpjs = ["45997418000153", "17140820000262"]
+
+displayDadosCep("29665000")
+displayDadosCnpj(cnpjs)
